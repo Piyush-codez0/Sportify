357ac9a6d58b9d9069c0862ea9b7ce48547e37aa
Piyush-codez0 <piyushcode.z1@gmail.com>
2025-12-16 21:04:17 +0530

    Added a loading screen

---
+++ components/LoadingScreen.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+
+const Loader = () => {
+  return (
+    <StyledWrapper>
+      <svg
+        width={240}
+        height={240}
+        viewBox="0 0 240 240"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="loader"
+      >
+        <defs>
+          <path
+            id="move-path"
+            d="M102.546 83.5C109.859 70.8333 128.141 70.8333 135.454 83.5L157.971 122.5C165.284 135.167 156.143 151 141.517 151H96.4833C81.8571 151 72.7158 135.167 80.0289 122.5L102.546 83.5Z"
+            fill="#D9D9D9"
+          />
+          <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" stdDeviation={5} result="blur" />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 60 -32"
+            />
+          </filter>
+        </defs>
+        <g filter="url(#goo)">
+          <circle cx={119} cy={74} r={20} stroke="#6f8191" strokeWidth={8} />
+          <circle cx={79} cy={141} r={20} stroke="#6f8191" strokeWidth={8} />
+          <circle cx={157} cy={141} r={20} stroke="#6f8191" strokeWidth={8} />
+          <circle cx={0} cy={0} r={14} fill="#6f8191">
+            <animateMotion
+              path="M102.546 83.5C109.859 70.8333 128.141 70.8333 135.454 83.5L157.971 122.5C165.284 135.167 156.143 151 141.517 151H96.4833C81.8571 151 72.7158 135.167 80.0289 122.5L102.546 83.5Z"
+              dur="2s"
+              repeatCount="indefinite"
+            />
+          </circle>
+        </g>
+      </svg>
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+
+  .loader {
+    height: 240px;
+    width: 240px;
+  }
+`;
+
+export default Loader;
